83a989110e717236514e5b79deb64243c848d7ff
KumbharPallavi <61708558+KumbharPallavi@users.noreply.github.com>
2025-07-07 12:09:28 +0000

    basic datatypes and variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "pallavi@google.com"
+var accountPassword = "12345"
+let accountState;
+/* prefere not use var
+*/
+accountCity = "Jaipur"
+
+// accountId = 2   //not allowed
+
+accountEmail = "pk.pk@google.com"
+accountPassword="21214"
+accountCity = "Banglore"
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
+console.log(accountId);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs and not browser
+
+console.log(3+3)
+
+console.log("India");
+
+let name = "Aditi"
+let age = "18"
+let isLoggedIn = true;
+
+//no => 1 to the power of 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not defined yet
+//symbol => unique
+
+//object
+
+console.log(typeof("India"))
+console.log(typeof(null))
+console.log(typeof(undefined))
