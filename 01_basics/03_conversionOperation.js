2d1007bb4a4fe8d5c85deb21f5989cbbdc54606f
KumbharPallavi <61708558+KumbharPallavi@users.noreply.github.com>
2025-07-08 08:02:46 +0000

    discussion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1
+//false => 0
+//null => 0
+//undefined => Nan
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
